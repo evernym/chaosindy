3734b6e24f272050f84fe0d01f7a877e36e9e048
Devin Fisher <devin.fisher@evernym.com>
2018-05-30 12:27:34 -0600

    Second cut - add support for ssh config
    Signed-off-by: Devin Fisher <devin.fisher@evernym.com>

--- chaosindy/execute/execute.py
+++ chaosindy/execute/execute.py
@@ -2,69 +2,73 @@ import abc
 import os
 
 import sys
-from fabric import Connection
+from collections import namedtuple
+
+from fabric import Connection, Config
+from paramiko import AuthenticationException
+
+Result = namedtuple('Result', ['return_code', 'stdout', 'stderr'])
 
 
 class RemoteExecutor(object):
     __metaclass__ = abc.ABCMeta
 
-    def execute(self, host: str, action: str):
-        rtn = self._execute_on_host(host, action)
-        print(rtn)
-        pass
+    def execute(self, host: str, action: str, user: str=None, **kwargs):
+        rtn = self._execute_on_host(host, action, user=user, **kwargs)
+        return rtn
 
     @abc.abstractmethod
-    def _execute_on_host(self, host: str, action: str) -> str:
+    def _execute_on_host(self, host: str, action: str, user: str=None) -> str:
         raise NotImplementedError('users must define __str__ to use this base class')
 
 
 class FabricExecutor(RemoteExecutor):
-    pem_files = None
+    config = None
 
-    def __init__(self, pem_dir=None):
-        self.pem_files = FabricExecutor._find_pem_files(pem_dir)
+    def __init__(self, ssh_config_file=None):
+        self.config = FabricExecutor._create_config(ssh_config_file=ssh_config_file)
+        pass
+
+    # @staticmethod
+    # def _format_rtn(rtn):
+    #     return str(rtn)
 
     @staticmethod
-    def _format_rtn(rtn):
-        return str(rtn)
+    def _create_config(ssh_config_file=None):
+        if ssh_config_file:
+            FabricExecutor._is_readable_file(ssh_config_file, 'ssh_config')
+        return Config(runtime_ssh_path=ssh_config_file)
 
     @staticmethod
-    def _find_pem_files(d):
-        rtn = []
+    def _is_readable_file(path, file_kind):
+        if not isinstance(path, str):
+            raise ValueError("path to file must be a string")
 
-        if not os.path.exists(d):
-            print("Unable to pem directory", file=sys.stderr)
-            return None
+        if os.access(path, os.R_OK):
+            if os.path.isfile(path):
+                return
+            else:
+                raise OSError("Path is not to a file -- '%s'" % str(path))
+        else:
+            raise OSError("Unable to access the file (not readable) -- %s -- '%s'" % (file_kind, path))
 
-        d = os.path.abspath(d)
-
-        for root, dirs, files in os.walk(d):
-            def is_pem_file(rel_path):
-                path = os.path.join(root, rel_path)
-                try:
-                    with open(path, 'r') as f:
-                        head = f.read()
-                        if head.strip().startswith("-----BEGIN"):
-                            if 'sao' in root:
-                                return True
-                except:
-                    pass
-                return False
-
-            files = list(filter(is_pem_file, files))
-            for file in files:
-                rtn.append(os.path.join(root, file))
-
-        return rtn
-
-    def _execute_on_host(self, host: str, action: str) -> str:
+    def _execute_on_host(self, host: str, action: str, user: str=None, identity_file=None) -> str:
         connect_kwargs = {}
-        if self.pem_files:
-            connect_kwargs['key_filename'] = self.pem_files
+
+        if identity_file:
+            FabricExecutor._is_readable_file(identity_file, 'identity_file')
+            connect_kwargs['key_filename'] = identity_file
 
         if not connect_kwargs:
             connect_kwargs = None
-        rtn = Connection(host, user='ubuntu', connect_kwargs=connect_kwargs).sudo(action)
+        try:
+            rtn = self._do_execute_on_host(host, action, user=user, connect_kwargs= connect_kwargs)
+        except AuthenticationException as e:
+            raise e
 
-        return FabricExecutor._format_rtn(rtn)
+        return rtn
+
+    def _do_execute_on_host(self, host, action, user=None, connect_kwargs=None):
+        rtn = Connection(host, config=self.config, user=user, connect_kwargs=connect_kwargs).sudo(action)
+        return Result(rtn.return_code, rtn.stdout, rtn.stderr)
 
